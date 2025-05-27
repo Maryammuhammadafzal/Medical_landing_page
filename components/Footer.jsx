6878740643f71f95295b8241097528308c118c2f
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-27 23:12:23 +0500

    starting Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='w-full h-auto my-10 flex justify-start items-center'>
+      <div className='w-full md:w-[85%] h-auto flex justify-center items-center flex-col gap-6'>
+<div className='w-full flex items-start justify-start gap-40'>
+
+</div>
+<div className="copyright w-full h-auto py-3 flex justify-center items-center">
+  
+</div>
+      </div>
+    </footer>
   )
 }
 
