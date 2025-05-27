969ef292c7531877a0b035078ecebdba477f6ea9
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-27 16:26:58 +0500

    Half work done

--- app/Contact/Page.jsx
+++ app/Contact/Page.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import Map from "../../public/map.png";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 const ContactPage = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="w-full h-auto my-35 flex flex-col justify-center items-center">
+      <div className="lg:w-[90%] w-full flex md:flex-row flex-col gap-5 justify-center items-center h-auto">
+        <div className="img md:w-[50%] h-auto flex justify-center items-center">
+          <Card
+            className="flex justify-end flex-col relative items-center bg-cover bg-top-left xl:h-[600px] xl:w-[600px] h-[500px] w-[500px] rounded-lg"
+            style={{ backgroundImage: `url(${Map.src})` }}
+          >
+            <CardContent className="bg-[#A8D5BA] absolute top-0 left-0 min-h-[400px] text-white p-5 w-full flex flex-col justify-end rounded-lg items-center">
+              <h2 className="lg:text-5xl md:text-4xl text-3xl font-semibold  text-[#2E8B57]">
+                Contact
+              </h2>
+              <p className="text-2xl font-medium flex gap-2 text-black"><span className="text-xl font-bold text-black">Location:</span> [Insert Address] </p>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="content md:w-[50%] sm:w-[80%] w-full sm:p-0 px-5 h-auto flex flex-col justify-center items-start gap-4">
+          <h2 className="lg:text-5xl md:text-4xl text-3xl font-semibold  text-[#2E8B57]">
+            Get In Touch
+          </h2>
 
-export default ContactPage
+          <div className="button w-full h-auto flex justify-center items-center ">
+            <Button className="bg-[#A8D5BA] py-6 px-8 rounded-lg text-[#2E8B57] text-base font-bold hover:bg-[#b7e7ca] ">
+              Book Your Appointment
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
--- app/Team/Page.jsx
+++ app/Team/Page.jsx
@@ -1,33 +1,54 @@
 import { Card, CardContent } from '@/components/ui/card'
 import React from 'react'
-
+import doctor1 from "../../public/doctor-1.jpg"
+import doctor2 from "../../public/doctor-2.jpg"
+import doctor3 from "../../public/doctor-3.jpg"
+import doctor4 from "../../public/doctor-4.jpg"
 const TeamPage = () => {
         let doctors_data = [
                 {
-                        
-                }
+                        doctor_image: doctor1,
+                        doctor_name: "Dr. Emily Carter",
+                        doctor_title: "Internal Medicine"
+                },
+                {
+                        doctor_image: doctor2,
+                        doctor_name: "Dr. Sophia Nguyen",
+                        doctor_title: "Cardiology"
+                },
+                {
+                        doctor_image: doctor3,
+                        doctor_name: "Dr. David Kim",
+                        doctor_title: "Women's Health"
+                },
+                {
+                        doctor_image: doctor4,
+                        doctor_name: "Internal Medicine",
+                        doctor_title: "Pediatrics"
+                },
         ]
   return (
   <div className="w-full h-auto my-25 flex flex-col justify-center items-center ">
       <div className="xl:w-[90%] w-full flex  flex-col justify-center gap-14 items-center h-auto">
-        <h2 className="lg:text-5xl md:text-4xl text-3xl font-semibold  text-[#2E8B57]">
-          Our Services
+        <h2 className="lg:text-5xl text-4xl font-semibold  text-[#2E8B57]">
+          Our Team
         </h2>
       
       <div className="cards flex justify-center gap-6 max-lg:flex-wrap items-center w-full h-auto">
-        {service_data &&
-          service_data.map(({ service_name } , index) => (
-            <Card key={index} className="bg-[#A8D5BA] flex justify-center items-center h-[200px] w-[300px] ">
-              <CardContent className=" text-[#2E8B57] text-center w-full flex flex-col gap-6 justify-center items-center">
-                {
-                index === 0 && (<FaStethoscope size={45} />) ||
-                index === 1 && (<BsFillChatHeartFill size={45} />) ||
-                index === 2 && (<FaMaskFace size={45} />) ||
-                index === 3 && (<IoBagAddSharp size={45} />) 
-                  }
-                <p className="font-bold text-2xl md:text-3xl">{service_name}</p>
-              </CardContent>
-            </Card>
+        {doctors_data &&
+          doctors_data.map(({ doctor_image , doctor_name , doctor_title } , index) => (
+            <Card key={index}
+                       className="flex justify-end flex-col relative items-center bg-cover bg-center h-[400px] w-[300px] rounded-lg"
+                       style={{ backgroundImage: `url(${doctor_image.src})` }}
+                     >
+                       <CardContent className="bg-[#A8D5BA] text-[#2E8B57] absolute bottom-0 left-0 min-h-[90px] rounded-lg p-5 w-full flex flex-col justify-end items-center">
+                      <h3 className="font-bold text-xl md:text-2xl">{doctor_name}</h3>
+                         <p className="font-semibold text-sm text-white">
+                          {doctor_title}
+                         </p>
+
+                       </CardContent>
+                     </Card>
           ))}
       </div>
       </div>
